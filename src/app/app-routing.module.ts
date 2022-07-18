767d9dd878e63a1c82f495de7145dd83d91d9823
D1x <wluis@miumg.edu.gt>
2022-07-18 16:04:59 -0600

    primer_commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReporteAereolineasComponent } from './componente-reportes/reporte-aereolineas/reporte-aereolineas.component';
+import { ReporteAvionesAereolineaComponent } from './componente-reportes/reporte-aviones-aereolinea/reporte-aviones-aereolinea.component';
+import { ReporteDestinosAutorizadoComponent } from './componente-reportes/reporte-destinos-autorizado/reporte-destinos-autorizado.component';
+import { ReporteEquipajeComponent } from './componente-reportes/reporte-equipaje/reporte-equipaje.component';
+import { ReporteListadoVuelosComponent } from './componente-reportes/reporte-listado-vuelos/reporte-listado-vuelos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reporte-listado-vuelos',
+    component: ReporteListadoVuelosComponent
+  },
+  {
+    path: 'reporte-aerolineas',
+    component: ReporteAereolineasComponent
+  },
+  {
+    path: 'reporte-aviones-aereolinea',
+    component: ReporteAvionesAereolineaComponent
+  },
+  {
+    path: 'reporte-destinos-autorizado',
+    component: ReporteDestinosAutorizadoComponent
+  },
+  {
+    path: 'reporte-equipaje',
+    component: ReporteEquipajeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
